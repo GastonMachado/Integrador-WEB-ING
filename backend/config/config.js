357ac5a4f7cdc0c812f0bbe4decb01a7e116b235
GastonMachado <gastonmachado44@gmail.com>
2023-06-13 20:57:38 -0300

    n

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     database: {
       host: 'localhost', // Dirección del servidor de la base de datos
       port: 3306, // Puerto del servidor de la base de datos
-      user: 'usuario_mysql', // Nombre de usuario de la base de datos
-      password: 'contraseña_mysql', // Contraseña de la base de datos
+      user: 'root', // Nombre de usuario de la base de datos
+      password: 'Integrador2023', // Contraseña de la base de datos
       database: 'nombre_base_de_datos' // Nombre de la base de datos
     }
   };
